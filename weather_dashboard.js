59fe26d6997c71b8d8e117b7f6a9fd5f87ba3edf
Ben Cole <benedictcole1@gmail.com>
2020-04-01 17:57:16 +1030

    indexed into the api object. created new array

--- weather_dashboard.js
+++ weather_dashboard.js
@@ -65,41 +65,58 @@ $(document).ready(function () {
 
     let openWeather = { "coord": { "lon": 138.6, "lat": -34.93 }, "weather": [{ "id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04d" }], "base": "stations", "main": { "temp": 20.72, "feels_like": 17.24, "temp_min": 20, "temp_max": 22.78, "pressure": 1012, "humidity": 56 }, "visibility": 10000, "wind": { "speed": 5.7, "deg": 50 }, "clouds": { "all": 90 }, "dt": 1585715323, "sys": { "type": 1, "id": 9566, "country": "AU", "sunrise": 1585688283, "sunset": 1585730434 }, "timezone": 37800, "id": 2078025, "name": "Adelaide", "cod": 200 }
 
-    let getWeatherData = () => {
-        const city = openWeather["name"];
-        const icon = openWeather["main"];
-        const bgDiv = openWeather["description"];
-        const temp = openWeather["temp"];
-        const humidity = openWeather["humidity"];
-        const windSpeed = openWeather["speed"];
-        const windAngle = openWeather["degree"];
-        const weatherDataArray = [city, icon, bgDiv, temp, humidity, windSpeed, windAngle];
-        console.log(weatherDataArray);
 
-        // create button and store data
-        const btn = document.createElement("button");
-            btn.innerHTML = "CLICK ME";
-            document.body.appendChild(btn);
-        }
+    const city = openWeather['name'];
+    console.log(city)
+    const icon = openWeather['weather'][0]['main'];
+    console.log(icon)
+    const bgDiv = openWeather['weather'][0]['description'];
+    console.log(bgDiv)
+    const temp = openWeather['main']['temp'];
+    console.log(temp)
+    const humidity = openWeather['main']['humidity'];
+    console.log(humidity)
+    const windSpeed = openWeather['wind']['speed'];
+    console.log(windSpeed)
+    const windAngle = openWeather['wind']['deg'];
+    console.log(windAngle)
 
-        const createButton = $("#city").on('click', function () {
+    const weatherDataArray = [city, icon, bgDiv, temp, humidity, windSpeed, windAngle];
+
+    console.log(weatherDataArray);
+
+
+    /* let getWeatherData = () => {
+     const weatherData = openWeather[{'name'} , {'main'}];const weatherData = openWeather.filter( function(item){return (item == clouds);} );
+     console.log(weatherData);
+ } */
 
 
 
+    /* create button and store data
+    $("#query-submit-button").click(function () {
+        let cityButton = $('<button/>', {
+            text: 'yo',
+            click: function () { alert('hello twat'); }
+        });
+
+        var parent = $('#new-button-row').children().append(cityButton).end();
+    });
+
+    $(this).find(".saveBtn").on('click', function() {
         let inputText = inputElement.val();
         console.log(inputText);
         // local storage experiment
         localStorage.setItem('slot' + currentSlot, inputText);
         console.log(localStorage);
     })
-
-}
-
-getWeatherData();
+    */
 
 
 
-})
+
+});
+
 
 
 
