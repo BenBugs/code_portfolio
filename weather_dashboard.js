38314c0be8172b07bf46b08a9c0699475a407089
Ben Cole <benedictcole1@gmail.com>
2020-04-01 13:04:40 +1030

    using dummy api file

--- weather_dashboard.js
+++ weather_dashboard.js
@@ -1,14 +1,22 @@
 // load document HTML
 $(document).ready(function () {
 
+    /*initialze query form
+    let cityQuery = () => {
+        let check = document.getElementById("#enter-city").value;
+        let inputVal = "";
+        if (check) {
+            inputVal = input.value;
+        }
+    }
 
     //api key and query string
-    let cityQuery = document.getElementById("#enter-city").value;
-    const api_key = `0d00e06c2b9381d1603d8240efcc25fb`; 
-    let query_url = `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery}&appid=${api_key}&units=metric`;
+    const api_key = `0d00e06c2b9381d1603d8240efcc25fb`;
+    let query_url = `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery()}&appid=${api_key}&units=metric`;
 
 
-    const api_call = $('#query-submit-button').click(function() { 
+
+    const api_call = $('#query-submit-button').click(function () {
         console.log(query_url);
         $.ajax({
             url: query_url,
@@ -23,6 +31,25 @@ $(document).ready(function () {
             }
         });
     });
+    */
+
+
+    let openWeather =  { "coord": { "lon":9.19, "lat":45.46}, "weather": [{ "id":803, "main":"Clouds", "description":"broken clouds", "icon":"04n"}], "base":"stations", "main": { "temp":3.69, "feels_like":0.93, "temp_min":1, "temp_max":5.56, "pressure":1024, "humidity":74}, "visibility":10000, "wind": { "speed":1, "deg":40}, "clouds": { "all":73}, "dt":1585703463, "sys": { "type":1, "id":6742, "country":"IT", "sunrise":1585717372, "sunset":1585763449}, "timezone":7200, "id":3173435, "name":"Milan", "cod":200}
+//call moment.js day and time
+
+    const getWeatherData = () => {
+    let city = openWeather["name"];
+    let icon = openWeather["main"];
+    let bgDiv = openWeather["description"];
+    let temp = openWeather["temp"];
+    let humidity = openWeather["humidity"] ;
+    let windSpeed = openWeather["speed"];
+    let windAngle = openWeather["degree"];
+    console.log(city + icon + bgDiv + temp + humidity + windSpeed + windAngle');
+    }
+
+    getWeatherData();
+
 
 
     //let today = api_call.json.weather.date;
@@ -32,7 +59,6 @@ $(document).ready(function () {
     //let uv = api_call.json.weather.uv;
 
 
-// dummy APi call {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}
 
     //$('#clear-results-button');
 
@@ -55,7 +81,7 @@ let buttonEl = document.querySelector(".button");
 //Edomam
 // let API_Key = "932a358d75a5f63deb3ecf3297f01302";
 // let appID = "d8bd0342";
-// WHISK 
+// WHISK
 // let API_Key =  "WeqsV4dzDqQZkiTKsnog6L3E8FdaFAlzJIan00T5itFAs7CWCU1acCq4YokSKgrR"
 const queryAPI = () => {
     let queryParam = inputEl.value;
---
+++ weather_dashboard_2.js
@@ -0,0 +1,19 @@
+// load document HTML
+$(document).ready(function () {
+
+
+
+
+
+    //let today = api_call.json.weather.date;
+    //let city = api_call.json.weather.city;
+    //let temp = api_call.json.weather.celsius;
+    //let speed = api_call.json.weather.wind_speed;
+    //let uv = api_call.json.weather.uv;
+
+
+    // dummy APi call {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}
+
+    //$('#clear-results-button');
+
+});
