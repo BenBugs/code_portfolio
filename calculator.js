5e74befdb388ae4fc213aabb4e379816b752a095
Ben Cole <benedictcole1@gmail.com>
2020-04-14 23:25:34 +0930

    working number input string

--- calculator.js
+++ calculator.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
 
+    let inputNum= ["",""];
+    const numIndex = 0; 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $('.number-button').on("click", (event)=> {
+        inputNum[numIndex] += event.currentTarget.value;
+        console.log(inputNum);
+        $('#display-sum').text(inputNum[numIndex]);
+      })
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // function(){
+    //    let number = ($(this).val());
+    //    console.log(number);
+    //    inputNum[numIndex] += e.currentTarget.value;
+    //    (e)=> {
+    //     numbers[currentIndex] += e.currentTarget.value;
+    //     console.log(numbers);
+    // });
+
+
+    // function compileSum() {
+
+    // }
+
+
+
+
+
+
 });
